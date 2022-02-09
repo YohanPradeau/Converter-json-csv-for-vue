310e8d8e618bf0248976eeeb7c80474b2263680e
Yohan <yohan.pradeau@agriware.com>
2022-02-09 11:19:33 +0100

    The JSON to CSV now require a new arg : source. It represent the source language and is used to show the data in the following format to ease the translators work : key | source language | translation word| english         | anglais

--- converter.py
+++ converter.py
@@ -11,24 +11,28 @@ from flatten_json import unflatten
 
 #Arguments
 argparser = argparse.ArgumentParser()
-argparser.add_argument("file", type=str, help="The file to convert")
+argparser.add_argument("source",  nargs='?', type=str, help="The source language (only used when you want to json to csv)" ,default="no_path")
+argparser.add_argument("target", type=str, help="The file to convert")
 
 args = argparser.parse_args()
 path = os.path.dirname(__file__)
-fullpath = os.path.join(os.path.dirname(__file__), args.file)
-file = args.file
-filename = os.path.basename(args.file)
-type = file[file.rfind('.') + 1::]
+fullpathTarget = os.path.join(os.path.dirname(__file__), args.target)
+fileTarget = args.target
+if args.source != "no_path" :
+    fullpathSource = os.path.join(os.path.dirname(__file__), args.source)
+    fileSource = args.source
+
+filename = os.path.basename(args.target)
+type = fileTarget[fileTarget.rfind('.') + 1::]
 
 
 def csv_to_json(): #Function for the csv to json conversion
     try:
-        # READING JSON FILE
+        # READING JSON file
         data = {}
-        with open(fullpath, encoding='utf-8') as f:
+        with open(fullpathTarget, encoding='utf-8') as f:
             csv_dict_reader = csv.DictReader(f)
             for row in csv_dict_reader:
-                # print(row['Parent (do not edit)']+"."+row["Translation"])
                 data[row['Parent (do not edit)']] = row["Translation"]
             data = unflatten(data,'.')
 
@@ -40,32 +44,37 @@ def csv_to_json(): #Function for the csv to json conversion
         
         print("Done ! \nYou can check the file at the following path : "+ outputPath)
 
-    except Exception:
-        print("Error : "+Exception)
+    except Exception as e:
+        print("Error : "+type(e))
 
 def json_to_csv(): #Function for the json to csv conversion
     try:
-        with open(fullpath,"r",encoding="utf-8") as f:
-            file = json.load(f)
-        print(fullpath)
+        with open(fullpathSource,"r",encoding="utf-8") as f:
+            fileSource = pandas.json_normalize(json.loads(f.read()))
+        with open(fullpathTarget,"r",encoding="utf-8") as f:
+            fileTarget = pandas.json_normalize(json.loads(f.read()))
+        df = pandas.concat([fileTarget, fileSource], ignore_index=True)
+
         with open(path+"\convertedFiles\\"+filename+".csv", 'w', encoding="utf-8", newline='') as output:
             csvwriter = csv.writer(output)
-            fr = ["Parent (do not edit)", "Translation"]
+            fr = ["Parent (do not edit)", "Source language" ,"Translation"]
             csvwriter.writerow(fr)
 
-            df = pandas.json_normalize(file)
+            pandas.set_option("display.max_rows", None, "display.max_columns", None)
             for elem in df:
                 row = []
                 parent = elem
+                # print(df[elem])
+                src = df[elem][1]
                 tl = df[elem][0]
                 row.append(parent)
+                row.append(src)
                 row.append(tl)
                 csvwriter.writerow(row)
         print("Done ! \nYou can check the file at the following path : "+ path+"\convertedFiles\\"+filename+".csv")
 
             
-    except Exception:
-        print("File not found : "+fullpath)
+    except Exception as e: print(repr(e))
 
 #Prepare the folder for converted files
 Path("./convertedFiles").mkdir(parents=True, exist_ok=True)
@@ -87,19 +96,22 @@ if type == "csv":
     else:
         csv_to_json()
 elif type == "json":
-    outputPath=path+"\convertedFiles\\"+filename+".csv"
-    fileExist = Path(outputPath).is_file()
-    if fileExist:
-        print("There is already a file at the output location with this exact same name and path ("+path+"\convertedFiles\\"+filename[:-4]+")")
-        reply = str(input("Are you sure you wish to OVERWRITE it ? "+' (y/n): ')).lower().strip()
-        if reply[0] == 'y':
-            json_to_csv()
-        elif reply[0] == 'n':
-            print("Task canceled")
+    if args.source != "no_path":
+        outputPath=path+"\convertedFiles\\"+filename+".csv"
+        fileExist = Path(outputPath).is_file()
+        if fileExist:
+            print("There is already a file at the output location with this exact same name and path ("+path+"\convertedFiles\\"+filename[:-4]+")")
+            reply = str(input("Are you sure you wish to OVERWRITE it ? "+' (y/n): ')).lower().strip()
+            if reply[0] == 'y':
+                json_to_csv()
+            elif reply[0] == 'n':
+                print("Task canceled")
+            else:
+                print('Invalid input, task canceled.\nPlease be sure to answer with "y" or "n"')
         else:
-            print('Invalid input, task canceled.\nPlease be sure to answer with "y" or "n"')
+            json_to_csv()
     else:
-        json_to_csv()
+        print("Source language argument is needed for the json -> csv operation.")
     
 else:
     print("Only CSV and JSON file type are accepted.")
